c88fd7b9900d06d981f9412c5a7fc12ec282a0a9
noehparrales <146775845+noehparrales@users.noreply.github.com>
2023-11-25 00:54:59 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 set -e
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
 
-# java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore 
+java -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore 
